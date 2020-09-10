ca455d7619ba1678ed3497bde6c635c99e4950f7
jrarkaan <rakajanitraa@hotmail.com>
2020-09-10 17:33:41 +0700

    menginstall material-ui-core, melempat data API ke props, dan mengkoding Card.jsx

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+// importing style
+import styles from './Card.module.css';
 
-const Cards = ()=>{
+const Cards = ({ confirmed, recovered, deaths, lastUpdate })=>{
   return(
-    <h1>Cards</h1>
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card}>
+          <Typography color="textSecondary" gutterBottom>Infected</Typography>
+          <Typography variant="h5">REAL DATA</Typography>
+          <Typography color="textSecondary">REAL DATE</Typography>
+          <Typography variant="body2">Number of active cases of COVID-19</Typography>
+        </Grid>
+        <Grid item component={Card}>
+          <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+          <Typography variant="h5">REAL DATA</Typography>
+          <Typography color="textSecondary">REAL DATE</Typography>
+          <Typography variant="body2">Number of recoveries from COVID-19</Typography>
+        </Grid>
+        <Grid item component={Card}>
+          <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+          <Typography variant="h5">REAL DATA</Typography>
+          <Typography color="textSecondary">REAL DATE</Typography>
+          <Typography variant="body2">Number of deaths caused by COVID-19</Typography>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
 
