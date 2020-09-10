c16f4fbdf9dbf9efdb08d2fe58b95de7346c0288
jrarkaan <rakajanitraa@hotmail.com>
2020-09-10 18:33:02 +0700

    memperbaiki beberapa bug

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,13 +3,16 @@ import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 // importing style
 import styles from './Card.module.css';
 
-const Cards = ({ data })=>{
+const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} })=>{
+  if(!confirmed){
+    return 'Loading...';
+  }
   return(
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
         <Grid item component={Card}>
           <Typography color="textSecondary" gutterBottom>Infected</Typography>
-          <Typography variant="h5">REAL DATA</Typography>
+          <Typography variant="h5">{confirmed.value}</Typography>
           <Typography color="textSecondary">REAL DATE</Typography>
           <Typography variant="body2">Number of active cases of COVID-19</Typography>
         </Grid>
