bb8ce6ef61c94d41e7e44c52df348d3f9694512a
jrarkaan <rakajanitraa@hotmail.com>
2020-09-11 13:49:16 +0700

    memperbaiki bug

--- src/App.js
+++ src/App.js
@@ -8,7 +8,21 @@ import styles from './App.module.css';
 class App extends React.Component {
   state = {
     data: {},
+    country: '',
   }
+
+  handleCountryChange = async(country)=>{
+    console.log(country);
+    // fetch the data
+    const fetchedData = await fetchData(country);
+    console.log(fetchedData);
+    // set the state
+    this.setState({
+      data: fetchedData,
+      country: country
+    });
+  }
+
   async componentDidMount(){
     const fetchedData = await fetchData();
     this.setState({ data: fetchedData});
@@ -18,7 +32,7 @@ class App extends React.Component {
     return (
       <div className={styles.container}>
         <Cards data={data} />
-        <CountryPicker />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart />
       </div>
     );
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -2,12 +2,27 @@ import React, {Component, useState, useEffect} from 'react';
 import { NativeSelect, FormControl } from '@material-ui/core';
 //importing styles css
 import styles from './CountryPicker.module.css';
+//importing filess
+import { fetchCountries } from '../../api/index.js';
 
-const CountryPicker = ()=>{
+
+const CountryPicker = ({ handleCountryChange })=>{
+  const [fetchedCountries, setFetchedCountries] = useState([]);
+
+  useEffect(()=>{
+    const fetchAPI = async()=>{
+      setFetchedCountries(await fetchCountries());
+    }
+    fetchAPI();
+  }, [setFetchedCountries]);
+  //console.log(fetchedCountries);
   return(
     <FormControl className={styles.FormControl}>
-      <NativeSelect>
+      <NativeSelect defaultValue="" onChange={(e)=> handleCountryChange(e.target.value)}>
         <option value="global">Global</option>
+        {fetchedCountries.map((country,i)=>(
+          <option key={i} value={country}>{country}</option>
+        ))}
       </NativeSelect>
     </FormControl>
   );
