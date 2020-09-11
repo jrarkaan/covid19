c9b8b0828c73e7d43c149dde0ce217da78390349
jrarkaan <rakajanitraa@hotmail.com>
2020-09-11 16:13:04 +0700

    mengkoding seperti biasa

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -5,14 +5,14 @@ import { Line, Bar } from 'react-chartjs-2';
 //importing styles
 import styles from './Chart.module.css';
 
-const Chart = ()=>{
+const Chart = ({data, country})=>{
   const [dailyData, setDailyDate] = useState({});
   useEffect(()=>{
     const fetchApi = async()=>{
       setDailyDate(await fetchDailyData());
     }
     fetchApi();
-  });
+  },[]);
 
   const lineChart = (
     dailyData.length
@@ -36,10 +36,35 @@ const Chart = ()=>{
         />
     ) : null
   );
+  console.log(data.confirmed, data.recovered, data.deaths);
+  const barChart = (
+    data.confirmed
+      ? (
+        <Bar
+          data={{
+            labels: ['Infected', 'Recovered', 'Deaths'],
+            datasets: [{
+              label: 'People',
+              backgroundColor:[
+                'rgba(0, 0, 255, 0.5)',
+                'rgba(0, 255, 0, 0.5)',
+                'rgba(255, 0, 0, 0.5)'
+              ],
+              data: [data.confirmed.value, data.recovered.value, data.deaths.value]
+            }]
+          }}
+          options={{
+            legend: {display: false},
+            title: {display: true, text:`Current state in ${country}`},
+          }}
+
+        />
+      ) : null
+  )
 
   return(
     <div className={styles.container}>
-      {lineChart}
+      {country ? barChart : lineChart}
     </div>
   );
 }
