651317df1345fce673b1a14a4ff34eddf61777e3
jrarkaan <rakajanitraa@hotmail.com>
2020-09-11 21:07:24 +0700

    Updates

---
+++ precache-manifest.3d00134d8b8b365db1c37637321ddd6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb16957df73345477b4d17a9944c3df",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "36e7f69e65b5d906d59f",
+    "url": "/covid19/static/css/main.7ae2b839.chunk.css"
+  },
+  {
+    "revision": "427e8db5d20211c20ec8",
+    "url": "/covid19/static/js/2.d7ee0cfa.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid19/static/js/2.d7ee0cfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36e7f69e65b5d906d59f",
+    "url": "/covid19/static/js/main.6d0751e8.chunk.js"
+  },
+  {
+    "revision": "636bdfdab0a46f674c1f",
+    "url": "/covid19/static/js/runtime-main.09a13a41.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19/static/media/image.d7265326.png"
+  }
+]);
