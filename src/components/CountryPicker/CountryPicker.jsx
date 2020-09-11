bb1d50877dcc786312f17de29a2a93d7d8384cf9
jrarkaan <rakajanitraa@hotmail.com>
2020-09-11 09:52:11 +0700

    mengkoding file countrypicker

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {Component, useState, useEffect} from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
+//importing styles css
+import styles from './CountryPicker.module.css';
 
 const CountryPicker = ()=>{
   return(
-    <h1>CountryPicker</h1>
+    <FormControl className={styles.FormControl}>
+      <NativeSelect>
+        <option value="global">Global</option>
+      </NativeSelect>
+    </FormControl>
   );
 }
 
